503505ce1309f10a4145daf1bbd31cc8107c0c20
carmen-tm <carmentorremolina@gmail.com>
2019-05-13 17:23:06 +0200

    Create production version

---
+++ docs/precache-manifest.1f3b9f80802f110da1bd933a3d7a46ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187e63acc6c118ba3ab0efdfed105f76",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de7a4e69e44ef5d735a0",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "./static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "de7a4e69e44ef5d735a0",
+    "url": "./static/js/main.bf399636.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
