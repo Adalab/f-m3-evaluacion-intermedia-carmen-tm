65d2e893b5e2995ccbf05b429d96ccec9eb6b8fc
carmen-tm <carmentorremolina@gmail.com>
2019-05-13 17:53:07 +0200

    Paint all cards with basic structure and some styles

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import './Pokemon.css';
+
+// class Pokemon extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		//Bind my methods
+// 		this.paintItems = this.paintItems.bind(this);
+// 	}
+// 	//Class Methods
+// 	paintItems() {
+// 		const { data } = this.props;
+// 		console.log(data);
+// 		data.map((item, index) => {
+// 			return (
+// 				<div key={index} className="pokemon-card">
+// 					{/* <img src="" alt="" /> */}
+// 					<h2>{item.name}</h2>
+// 					<ul>
+// 						<li>my type</li>
+// 					</ul>
+// 				</div>
+// 			);
+// 		});
+// 	}
+
+// 	render() {
+// 		const { data } = this.props;
+// 		const { paintItems } = this;
+// 		return { paintItems };
+// 	}
+// }
+
+// export default Pokemon;
 
 class Pokemon extends React.Component {
+	// constructor(props) {
+	// 	super(props);
+	// 	//Bind my methods
+	// 	this.paintItems = this.paintItems.bind(this);
+	// }
+
 	render() {
-		return <div>mi cartita</div>;
+		const { data } = this.props;
+		console.log(data);
+		// const { paintItems } = this;
+		return data.map((item, index) => {
+			return (
+				<div key={index} className="pokemon-card">
+					<img src={item.url} alt="" />
+					<h2>{item.name}</h2>
+					<ul>
+						{item.types.map((item, index2) => {
+							return (
+								<li className="pokemon-type" key={index2}>
+									{item}
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			);
+		});
 	}
 }
 
