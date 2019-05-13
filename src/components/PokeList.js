a2cee94da28356bc43b5a7dc4b81d25bd9f69a5b
carmen-tm <carmentorremolina@gmail.com>
2019-05-13 18:30:37 +0200

    Clean code

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Pokemon from './Pokemon';
+import './PokeList.css';
 
 class PokeList extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Mi lista de pokemon</h1>
+				<h1 className="pokelist-title">Mi lista de pokemon</h1>
 				<ul>
 					<Pokemon data={this.props.data} />
 				</ul>
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,38 +1,5 @@
 import React from 'react';
 import './Pokemon.css';
-
-// class Pokemon extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-// 		//Bind my methods
-// 		this.paintItems = this.paintItems.bind(this);
-// 	}
-// 	//Class Methods
-// 	paintItems() {
-// 		const { data } = this.props;
-// 		console.log(data);
-// 		data.map((item, index) => {
-// 			return (
-// 				<div key={index} className="pokemon-card">
-// 					{/* <img src="" alt="" /> */}
-// 					<h2>{item.name}</h2>
-// 					<ul>
-// 						<li>my type</li>
-// 					</ul>
-// 				</div>
-// 			);
-// 		});
-// 	}
-
-// 	render() {
-// 		const { data } = this.props;
-// 		const { paintItems } = this;
-// 		return { paintItems };
-// 	}
-// }
-
-// export default Pokemon;
-
 class Pokemon extends React.Component {
 	constructor(props) {
 		super(props);
