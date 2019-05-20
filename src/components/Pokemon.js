2d55a76630b49046b9091ca73655507ae1168c97
carmen-tm <carmentorremolina@gmail.com>
2019-05-21 01:10:31 +0200

    Improve components logic

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -40,6 +40,31 @@ class Pokemon extends React.Component {
 				</div>
 			);
 		});
+
+		// return data.map((item, index) => {
+		// 	return (
+		// 		<div
+		// 			onClick={this.handleOnClick}
+		// 			title="Añádeme a tus favoritos"
+		// 			key={index}
+		// 			className="pokemon-card"
+		// 		>
+		// 			<img src={item.url} alt="Imagen de Pokemon" />
+		// 			<div className="title-container">
+		// 				<h2>{item.name}</h2>
+		// 			</div>
+		// 			<ul className="pokemon-type__list">
+		// 				{item.types.map((item, index2) => {
+		// 					return (
+		// 						<li className="pokemon-type__item" key={index2}>
+		// 							{item}
+		// 						</li>
+		// 					);
+		// 				})}
+		// 			</ul>
+		// 		</div>
+		// 	);
+		// });
 	}
 }
 
