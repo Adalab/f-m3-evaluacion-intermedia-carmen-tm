3d998d92d124989b35ec57bc35d29e6d545cbf42
carmen-tm <carmentorremolina@gmail.com>
2019-05-21 01:13:04 +0200

    Revert "Improve components logic"
    This reverts commit 2d55a76630b49046b9091ca73655507ae1168c97.

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -4,34 +4,8 @@ import './PokeList.css';
 
 class PokeList extends React.Component {
 	render() {
-		const { data } = this.props;
 		return (
 			<ul className="pokemon-list">
-				{/* {data.map((item, index) => {
-					return (
-						<li
-							onClick={this.handleOnClick}
-							title="Añádeme a tus favoritos"
-							key={index}
-							className="pokemon-card"
-						>
-							<img src={item.url} alt="Imagen de Pokemon" />
-							<div className="title-container">
-								<h2>{item.name}</h2>
-							</div>
-							<ul className="pokemon-type__list">
-								{item.types.map((item, index2) => {
-									return (
-										<li className="pokemon-type__item" key={index2}>
-											{item}
-										</li>
-									)}
-								})}
-							</ul>
-				</li>
-			);
-		}); */}
-
 				<Pokemon data={this.props.data} />
 			</ul>
 		);
